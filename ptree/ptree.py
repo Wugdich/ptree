1e514a3875a8db0e20a4ff97dcd10ef0d38dcc11
Wugd <sintes4ek@gmail.com>
2023-04-03 21:14:42 +0300

    Implement low-level _TreeGenerator class

--- ptree/ptree.py
+++ ptree/ptree.py
@@ -12,10 +12,69 @@ SPACE_PREFIX = "    "
 
 
 class DirectoryTree:
-    def __init__(self, root_dir):
+    """Class creates the directory tree diagram 
+    and display it on the screen.
+    """
+    def __init__(self, root_dir: str):
         self._generator = _TreeGenerator(root_dir)
 
     def generate(self):
+        """Displays each component of the tree diagram on the screen"""
         tree = self._generator.build_tree()
         for entry in tree:
             print(entry)
+
+
+class _TreeGenerator:
+    """Class traverses the file system and generates the directory tree
+    diagram.
+    """
+    def __init__(self, root_dir: str):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = list()
+
+    def build_tree(self) -> list[str]:
+        """Generates and returns the directory tree diagram."""
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        """Build a head of the tree."""
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
+
+    def _tree_body(self, directory: pathlib.Path, prefix: str=""):
+        """Build a body of the tree."""
+        entries = directory.iterdir()
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                        entry, index, entries_count, prefix, connector
+                        )
+            else:
+                self._add_file(entry, prefix, connector)
+
+    def _add_directory(
+            self, directory: pathlib.Path, index: int, 
+            entries_count: int, prefix: str, connector: str
+            ):
+        """Appends a new directory to the list represented a tree diagram."""
+        self._tree.append(f"{prefix}{connector} {directory.name}{os.sep}")
+        if index != entries_count - 1:
+            prefix += PIPE_PREFIX
+        else:
+            prefix += SPACE_PREFIX
+        self._tree_body(
+                directory=directory,
+                prefix=prefix
+                )
+        self._tree.append(prefix.rstrip())
+
+    def _add_file(self, file: pathlib.Path, prefix: str, connector: str):
+        """Appends a new file to the list represented a tree diagram."""
+        self._tree.append(f"{prefix}{connector} {file.name}")
+
