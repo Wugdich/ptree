927887dbcebe409b6438244b74132412bd3015f3
Wugd <sintes4ek@gmail.com>
2023-04-03 20:21:53 +0300

    Add the high-level DirectoryTree class

--- ptree/ptree.py
+++ ptree/ptree.py
@@ -9,3 +9,13 @@ ELBOW = "└──"
 TEE = "├──"
 PIPE_PREFIX = "│   "
 SPACE_PREFIX = "    "
+
+
+class DirectoryTree:
+    def __init__(self, root_dir):
+        self._generator = _TreeGenerator(root_dir)
+
+    def generate(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
